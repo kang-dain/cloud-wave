c22306437610438467f48069b8465adf4c9c05d7
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2025-07-24 08:40:34 +0000

    fix: empty PID

--- scripts/stop_streamlit.sh
+++ scripts/stop_streamlit.sh
@@ -14,16 +14,20 @@
 # exit 0
 
 
-#!/bin/bash
 
+
+
+#!/bin/bash
 APP_PID=$(pgrep streamlit)
 
 if [ -z "$APP_PID" ]; then
   echo "Application is not running"
-  exit 0   # ← 이걸 여기서 해주면 확실해져
 else
-  echo "Kill -9 $APP_PID"
+  echo "Killing streamlit process: $APP_PID"
   kill -9 $APP_PID
   sleep 5
-  exit 0
 fi
+
+exit 0
+
+
